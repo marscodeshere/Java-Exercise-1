505865a562ceb640a011617aa8a886524820adb3
marscodeshere <102072700+marscodeshere@users.noreply.github.com>
2024-09-24 17:44:06 -0400

    Update Poem.java
    Added comments for the code to understand the steps of the project.

--- Poem.java
+++ Poem.java
@@ -1,7 +1,11 @@
 public class Poem {
     public static void main(String[] args) {
+        //create array of stanzas for the poem
         String[] stanzas = {"Roses are red", "Violets are blue", "Sugar is sweet", "And so are you"};
+        
+        //cycle through array to access
         for(int i = 0; i < stanzas.length; i++)
+            //print to console on a new line each time
             System.out.println(stanzas[i]);
     }
 }
