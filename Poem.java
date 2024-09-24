14f53fe0a299fcfef0716178a9404742b0f16233
marscodeshere <102072700+marscodeshere@users.noreply.github.com>
2024-09-24 17:16:14 -0400

    Create Poem.java
    Simple program creation of an array of strings that form together to make a poem.

---
+++ Poem.java
@@ -0,0 +1,7 @@
+public class Poem {
+    public static void main(String[] args) {
+        String[] stanzas = {"Roses are red", "Violets are blue", "Sugar is sweet", "And so are you"};
+        for(int i = 0; i < stanzas.length; i++)
+            System.out.println(stanzas[i]);
+    }
+}
